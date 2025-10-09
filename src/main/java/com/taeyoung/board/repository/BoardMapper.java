8a8e387c161fa5f4f1ddc2c55997ae5b641bf34c
LeeTaeYoung <xodud5080@naver.com>
2025-10-09 13:30:50 +0900

    create mapper

---
+++ src/main/java/com/taeyoung/board/repository/BoardMapper.java
@@ -0,0 +1,15 @@
+package com.taeyoung.board.repository;
+
+import com.taeyoung.board.domain.Board;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+    Board save(Board board);
+    List<Board> findAll();
+    void deleteById(Long id);
+    Board findById(Long id);
+    void update(Board board);
+}
