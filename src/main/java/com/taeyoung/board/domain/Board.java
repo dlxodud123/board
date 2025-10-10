1082acc0e620847c2f8be435bf72ad30f7b08ae6
LeeTaeYoung <xodud5080@naver.com>
2025-10-10 11:24:51 +0900

    jpa domain

--- src/main/java/com/taeyoung/board/domain/Board.java
+++ src/main/java/com/taeyoung/board/domain/Board.java
@@ -1,13 +1,24 @@
 package com.taeyoung.board.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
     private String writer;
+
+    protected Board(){
+    }
 
     public Board(String title, String content, String writer) {
         this.title = title;
